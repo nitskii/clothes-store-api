5be84f3cb1d1f7302412121c321b1c3d54134f6d
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-12-20 14:11:05 +0200

    removed default app controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ClothesModule } from './clothes/clothes.module';
 import { PrismaModule } from './prisma/prisma.module';
 
@@ -8,8 +6,6 @@ import { PrismaModule } from './prisma/prisma.module';
   imports: [
     PrismaModule,
     ClothesModule
-  ],
-  controllers: [AppController],
-  providers: [AppService]
+  ]
 })
 export class AppModule {}
